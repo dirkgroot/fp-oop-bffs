22ed9a15ca9ebacafe0a8231f63c7455f9c1ea82
Dirk Groot <dirk.groot@avisi.nl>
2024-11-11 20:59:01 +0100

    Replace ProductName constructor with factory

--- src/main/kotlin/nl/avisi/shopping/cart/ProductName.kt
+++ src/main/kotlin/nl/avisi/shopping/cart/ProductName.kt
@@ -1,11 +1,17 @@
 package nl.avisi.shopping.cart
 
-data class ProductName(val value: String) {
-    init {
-        if (value.isBlank())
-            throw IllegalArgumentException("Product name must not be blank")
-        if (value.contains('\n'))
-            throw IllegalArgumentException("Product name must not contain newlines")
+import nl.avisi.shopping.Err
+import nl.avisi.shopping.Ok
+import nl.avisi.shopping.Status
+
+data class ProductName private constructor(val value: String) {
+    companion object {
+        fun of(value: String): Status<ProductName> =
+            when {
+                value.isBlank() -> Err("Product name must not be blank")
+                value.contains('\n') -> Err("Product name must not contain newlines")
+                else -> Ok(ProductName(value))
+            }
     }
 
     override fun toString() = value
